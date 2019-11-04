8917b7ba57e7d63766684afa44b0c9c265575c56
CarnunMP <carnun@hotmail.co.uk>
2019-11-04 14:06:51 +0000

    GET done

--- server.js
+++ server.js
@@ -6,4 +6,28 @@ const server = express();
 
 server.use(express.json());
 
+server.get('/accounts', (req, res) => {
+  db('accounts')
+    .then(accounts => {
+      res.status(200).json(accounts);
+    })
+    .catch(err => genericError(err));
+});
+
+server.get('/accounts/:id', (req, res) => {
+  const { id } = req.params;
+
+  db('accounts').where({ id })
+    .then(accounts => {
+      res.status(200).json(accounts);
+    })
+    .catch(err => genericError(err));
+});
+
+function genericError(err) {
+  return res.status(500).json({
+    message: "Error: " + err.message,
+  });
+}
+
 module.exports = server;
